ec2602386d189e4ca10757205b1454d10171eeb6
zeshantech <zeshanshakil0@gmail.com>
2023-11-05 13:51:26 +0500

    feat: upload-image-successfully

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,20 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CloudinaryOutputDto } from './dto/cloudinary.output.dto';
-import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
+import { UploadApiOptions, UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import { v4 as uuid } from 'uuid';
 import { Readable, Transform } from 'stream';
 import { CloudinaryInputDto } from './dto/cloudinary.input.dto';
 import { ConfigService } from '@nestjs/config';
-import { SingleBar, Presets } from 'cli-progress';
+import { SingleBar } from 'cli-progress';
 import { handleOnBase64ToBuffer } from '../utils/base64ToBuffer';
 
 @Injectable()
 export class CloudinaryService {
-  private readonly progressBar: any;
+  private readonly progressBar: SingleBar;
 
   constructor(private configService: ConfigService) {
     this.progressBar = new SingleBar({});
+
     cloudinary.config({
       cloud_name: this.configService.get('CLOUDINARY_CLOUD_NAME'),
       api_key: this.configService.get('CLOUDINARY_API_KEY'),
@@ -22,52 +22,48 @@ export class CloudinaryService {
     });
   }
 
-  async uploadImageOnCloudinary(input: CloudinaryInputDto.CloudinaryUploadInput): Promise<CloudinaryOutputDto.CloudinaryUploadOutput> {
-    try {
-      const uniqueImageId = uuid(); // Set a meaningful unique ID
+  private async uploadToCloudinary(options: UploadApiOptions, imageBuffer: Buffer): Promise<UploadApiResponse> {
+    return new Promise<UploadApiResponse>((resolve, reject) => {
       this.progressBar.start(100, 0);
 
-      const uploadStream = cloudinary.uploader
-        .upload_stream(
-          {
-            public_id: uniqueImageId,
-            resource_type: 'image',
-          },
-          (err, res) => {
-            if (err) throw new Error(err.message);
-            console.log(res, '');
-          },
-        )
-        .end((res) => {
-          console.log(res);
-        });
-
-      const imageBuffer = handleOnBase64ToBuffer(input.base64);
-      const imageStream = this.createImageStream(imageBuffer);
+      const uploadStream = cloudinary.uploader.upload_stream(options, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          this.progressBar.stop();
+          resolve(res);
+        }
+      });
 
       const progressTransform = new Transform({
-        transform(chunk, _, __) {
+        transform(chunk, _, callback) {
           this.emit('progress', chunk.length);
+          callback(null, chunk);
         },
       });
 
-      imageStream.pipe(progressTransform).pipe(uploadStream);
-
-      return new Promise<CloudinaryOutputDto.CloudinaryUploadOutput>((resolve, reject) => {
-        uploadStream.on('end', (e) => {
-          this.progressBar.stop();
-          console.log('Upload stream ended', e);
-          resolve({ imageUrl: e });
-        });
-
-        uploadStream.on('error', (error) => {
-          reject(error);
-        });
-
-        progressTransform.on('progress', (progress) => {
-          this.showProgress(imageBuffer.length, progress);
-        });
+      progressTransform.on('progress', (progress) => {
+        this.showProgress(imageBuffer.length, progress);
       });
+
+      const imageStream = this.createImageStream(imageBuffer);
+      imageStream.pipe(progressTransform).pipe(uploadStream);
+    });
+  }
+
+  async uploadImageOnCloudinary(input: CloudinaryInputDto.CloudinaryUploadInput): Promise<CloudinaryOutputDto.ImageUrlOutput> {
+    try {
+      const imageBuffer = handleOnBase64ToBuffer(input.base64);
+      const uniqueImageId = uuid();
+
+      const options: UploadApiOptions = {
+        public_id: uniqueImageId,
+        resource_type: 'image',
+      };
+
+      const res = await this.uploadToCloudinary(options, imageBuffer);
+
+      return { imageUrl: res.url };
     } catch (error) {
       throw new Error(error);
     }
